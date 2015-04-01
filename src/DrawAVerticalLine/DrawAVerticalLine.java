3295e2664681f1bba20766ff0847fe6420344484
David <davsucks@umich.edu>
2015-04-01 19:03:39 -0400

    Draw a vertical line

---
+++ src/DrawAVerticalLine/DrawAVerticalLine.java
@@ -0,0 +1,23 @@
+package DrawAVerticalLine;
+
+/**
+ * Created by David on 4/1/2015.
+ */
+public class DrawAVerticalLine {
+    public static void main(String[] args) {
+        int n;
+        try {
+            n = Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            alertUserToBadInput();
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            System.out.println("*");
+        }
+    }
+
+    private static void alertUserToBadInput() {
+        System.out.println("Please enter a positive integer at the command line");
+    }
+}
