183ac0d2a18eed58dc8320d596808da4923d79e4
David <davsucks@umich.edu>
2015-04-01 18:53:58 -0400

    Easiest Exercise Ever

---
+++ src/EasiestExerciseEver/EasiestExerciseEver.java
@@ -0,0 +1,10 @@
+package EasiestExerciseEver;
+
+/**
+ * Created by David on 4/1/2015.
+ */
+public class EasiestExerciseEver {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
